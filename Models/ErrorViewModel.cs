a1e001872dd54ed77fa6f9220fd3f313b55e8d45
HenryMixco <henry_daniel.9@hotmail.com>
2024-09-21 03:27:17 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace The_Temper_Trap_Desafio02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
